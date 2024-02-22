bd10be378d350fbb369783b22246bd52ac530dfa
BaDjinn <44167536+BaDjinn@users.noreply.github.com>
2024-02-22 15:38:59 +0100

    Update post.c

--- post.c
+++ post.c
@@ -1,7 +1,11 @@
-#include "helper.h"
 #include "post.h"
+#include <time.h>
 
 void handle_post(int client_socket, char *payload) {
-    printf("POST ricevuto: %s\n", payload);
+    time_t now = time(NULL);
+    char *time_str = ctime(&now);
+    time_str[strlen(time_str) - 1] = '\0';  // Rimuove il newline alla fine
+
+    printf("POST ricevuto: %s\nData e ora: %s\n", payload, time_str);
     // Qui potresti fare qualcosa con il payload, come salvarlo in un database
 }
