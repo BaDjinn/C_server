8c625dbc8ee4a643612d6fd90cd5c77c82a0fbe7
BaDjinn <44167536+BaDjinn@users.noreply.github.com>
2024-02-22 15:01:09 +0100

    Create patch.c

---
+++ patch.c
@@ -0,0 +1,6 @@
+#include "helper.h"
+
+void handle_patch(int client_socket, char *payload) {
+    printf("PATCH ricevuto: %s\n", payload);
+    // Qui potresti fare qualcosa con il payload, come aggiornare un record in un database
+}
