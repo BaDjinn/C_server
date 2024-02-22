120099ede0c529eab02fb874cea94f391745d43e
BaDjinn <44167536+BaDjinn@users.noreply.github.com>
2024-02-22 15:00:22 +0100

    Create post.c

---
+++ post.c
@@ -0,0 +1,6 @@
+#include "helper.h"
+
+void handle_post(int client_socket, char *payload) {
+    printf("POST ricevuto: %s\n", payload);
+    // Qui potresti fare qualcosa con il payload, come salvarlo in un database
+}
