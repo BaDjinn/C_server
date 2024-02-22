6581577c09ce3dea3efac57c3318cec76da98165
BaDjinn <44167536+BaDjinn@users.noreply.github.com>
2024-02-22 15:39:39 +0100

    Update patch.c

--- patch.c
+++ patch.c
@@ -1,7 +1,11 @@
-#include "helper.h"
 #include "patch.h"
+#include <time.h>
 
 void handle_patch(int client_socket, char *payload) {
-    printf("PATCH ricevuto: %s\n", payload);
+    time_t now = time(NULL);
+    char *time_str = ctime(&now);
+    time_str[strlen(time_str) - 1] = '\0';  // Rimuove il newline alla fine
+
+    printf("PATCH ricevuto: %s\nData e ora: %s\n", payload, time_str);
     // Qui potresti fare qualcosa con il payload, come aggiornare un record in un database
 }
