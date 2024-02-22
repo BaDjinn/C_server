1e7a2af3f50fd3964272ad3329aaee3d0daf9d6a
BaDjinn <44167536+BaDjinn@users.noreply.github.com>
2024-02-22 15:00:45 +0100

    Create get.c

---
+++ get.c
@@ -0,0 +1,14 @@
+#include "helper.h"
+#include <time.h>
+
+void handle_get(int client_socket, char *query) {
+    printf("GET ricevuto: %s\n", query);
+
+    time_t now = time(NULL);
+    char *time_str = ctime(&now);
+    time_str[strlen(time_str) - 1] = '\0';  // Rimuove il newline alla fine
+
+    char response[1024];
+    snprintf(response, sizeof(response), "HTTP/1.1 200 OK\nContent-Type: application/json\n\n{\"now\": \"%s\"}", time_str);
+    write(client_socket, response, strlen(response));
+}
