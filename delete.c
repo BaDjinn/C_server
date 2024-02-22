5cf1fdc3636c66dcdc0dec3001e626e40b3ec11e
BaDjinn <44167536+BaDjinn@users.noreply.github.com>
2024-02-22 15:39:52 +0100

    Update delete.c

--- delete.c
+++ delete.c
@@ -1,7 +1,11 @@
-#include "helper.h"
 #include "delete.h"
+#include <time.h>
 
 void handle_delete(int client_socket, char *payload) {
-    printf("DELETE ricevuto: %s\n", payload);
+    time_t now = time(NULL);
+    char *time_str = ctime(&now);
+    time_str[strlen(time_str) - 1] = '\0';  // Rimuove il newline alla fine
+
+    printf("DELETE ricevuto: %s\nData e ora: %s\n", payload, time_str);
     // Qui potresti fare qualcosa con il payload, come eliminare un record in un database
 }
